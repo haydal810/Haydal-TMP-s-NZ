801217d5759c3d6d72f718bff2fdea1ec8bb620e
David <haydal.tmps@gmail.com>
2020-01-09 22:23:42 +0000

    add EmailJS functionality

---
+++ js/sendEmail.js
@@ -0,0 +1,20 @@
+function sendMail(contact_form) {
+    emailjs.send("gmail", "haydal_tmps_website", {
+        "message": contact_form.message.value,
+        "from_name": contact_form.full_name.value,
+        "from_email": contact_form.emailaddress.value,
+        "nature_of_enquiry": contact_form.nature_of_enquiry.value,
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                setTimeout($("#modalMessage").modal(), 500); // the user will know that their message was sent successfully and is given feedback.
+                document.getElementById('contact_form').reset(); // the form is reset after submit button is hit
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page...
+}
+
